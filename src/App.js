593a1a972e79f98d778731225db8e39b3eaef89c
Castiel <castiel.andrew@gmail.com>
2018-07-26 23:53:57 -0700

    Added work.js, updated index.html, deployed to firebase, set up basic pwa environment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Router, Route } from 'react-router';
+import {
+  Link, BrowserRouter
+} from 'react-router-dom'
 import './App.css';
-
+const NavBar = () => (
+  <div className="navbar">
+    <Link to="/">Feed</Link>
+    <Link to="/profile">Profile</Link>
+  </div>
+);
+const Template = ({ title }) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      This is the {title} page.
+    </p>
+  </div>
+);
+const Feed = (props) => (
+  <Template title="Feed"/>
+);
+const Profile = (props) => (
+  <Template title="Profile"/>
+);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={BrowserRouter}>
+        <Route path="/" component={Feed}/>
+        <Route path="/profile" component={Profile}/>
+      </Router>
     );
   }
 }
-
 export default App;
