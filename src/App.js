257abd16e70c33638d6216857f43d0e992f62668
Castiel <castiel.andrew@gmail.com>
2018-08-20 23:02:38 -0700

    set-up-redux with simple action and reducer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
+/*
+ src/App.js
+*/
+import { simpleAction } from './actions/simple'
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
 import './App.css';
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+     simpleAction = (event) => {
+       this.props.simpleAction();
+     }
+ render() {
+
+  return (
+   <div className="App">
+    <button onClick={this.simpleAction}>Test redux action</button>
+    <pre>
+      {
+        JSON.stringify(this.props)
+      }
+</pre>
+   </div>
+   
+  );
+ }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+});
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+/*
+ src/reducers/rootReducer.js
+*/
+import { combineReducers } from 'redux';
+import simpleReducer from './simple';
+export default combineReducers({
+ simpleReducer
+});
